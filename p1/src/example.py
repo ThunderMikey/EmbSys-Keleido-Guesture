fc83f8c4d9c72f8c5336a39c663041614ec5cc08
ThunderMikey <mikecyj25@gmail.com>
2018-01-27 14:57:41 +0000

    new trial

--- p1/src/example.py
+++ p1/src/example.py
@@ -2,5 +2,8 @@ from machine import Pin, I2C
 
 i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
 i2cportNo = i2c.scan()
+ADSAddr = i2cportNo[0]
+data = i2c.readfrom_mem(ADSAddr, 0, 4)
 
 print(i2cportNo)
+print(data)
