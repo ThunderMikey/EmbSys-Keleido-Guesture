bd5f7ca0da5a4763342b2cc77cdb254b2ae287d9
ThunderMikey <mikecyj25@gmail.com>
2018-01-29 16:58:37 +0000

    successful read

--- p1/src/example.py
+++ p1/src/example.py
@@ -3,7 +3,14 @@ from machine import Pin, I2C
 i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
 i2cportNo = i2c.scan()
 ADSAddr = i2cportNo[0]
-data = i2c.readfrom_mem(ADSAddr, 0, 4)
+
+# write to config register 0x01
+# CONTINUOUS_READ=0000 010 0 100 0 0 0 11
+CONTINUOUS_READ=bytearray(0b0000010010000011)
+
+i2c.writeto_mem(ADSAddr, 1, CONTINUOUS_READ)
+data = i2c.readfrom_mem(ADSAddr, 0, 2)
 
 print(i2cportNo)
 print(data)
+print(int.from_bytes(data, 'big'))
