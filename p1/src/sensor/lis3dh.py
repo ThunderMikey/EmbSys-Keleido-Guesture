636d7ab66d6180a56a90131578b036d1370cad6d
zl6114 <zl6114@ic.ac.uk>
2018-02-12 17:49:35 +0000

    Lis3dh I2C set up

---
+++ p1/src/sensor/lis3dh.py
@@ -0,0 +1,61 @@
+from machine import Pin, I2C
+from umqtt.simple import MQTTClient
+#import system
+import network
+import ujson
+import time
+import webrepl
+import machine
+import ustruct
+# Register value constants:
+# refrenece to
+# RANGE_16_G               = const(0b11)    # +/- 16g
+# RANGE_8_G                = const(0b10)    # +/- 8g
+# RANGE_4_G                = const(0b01)    # +/- 4g
+# RANGE_2_G                = const(0b00)    # +/- 2g (default value)
+# DATARATE_1344_HZ         = const(0b1001)  # 1.344 KHz
+# DATARATE_400_HZ          = const(0b0111)  # 400Hz
+# DATARATE_200_HZ          = const(0b0110)  # 200Hz
+# DATARATE_100_HZ          = const(0b0101)  # 100Hz
+# DATARATE_50_HZ           = const(0b0100)  # 50Hz
+# DATARATE_25_HZ           = const(0b0011)  # 25Hz
+# DATARATE_10_HZ           = const(0b0010)  # 10 Hz
+# DATARATE_1_HZ            = const(0b0001)  # 1 Hz
+# DATARATE_POWERDOWN       = const(0)
+# DATARATE_LOWPOWER_1K6HZ  = const(0b1000)
+# DATARATE_LOWPOWER_5KHZ   = const(0b1001)
+
+# Other constants
+STANDARD_GRAVITY = 9.806
+
+class LIS3DH:
+    #Driver for LIS3DH accelerometer.
+    def __init__(self):
+        #set up I2C
+        self.i2c_lis3dh = I2C(scl=Pin(14), sda=Pin(15), freq=100000)
+        i2cportNo = self.i2c_lis3dh.scan()
+        ADSAddr = i2cportNo[0]
+        # Set Reboot flag of Control 5 Register (0x24) high
+        self.i2c_lis3dh.writeto_mem(ADSAddr,0x24,bytearray(0x80))
+        time.sleep(0.01) # takes 5ms
+
+
+    def acceleration(self):
+        i2cportNo = self.i2c_lis3dh.scan()
+        ADSAddr = i2cportNo[0]
+        # Enable all axes, normal mode, REG_CTRL1
+        self.i2c_lis3dh.writeto_mem(ADSAddr,0x20,bytearray(0x77))
+        # High res & BDU enabled. REG_CTRL4
+        # RANGE_2_G
+        self.i2c_lis3dh.writeto_mem(ADSAddr,0x23,bytearray(0x88))
+        # Enable ADCs.REG_TEMPCFG
+        self.i2c_lis3dh.writeto_mem(ADSAddr,0x1F,bytearray(0x80))
+        #The x, y, z acceleration values returned in a 3-tuple and are in m / s ^ 2."""
+        divider = 16380
+        x = self.i2c_lis3dh.readfrom_mem(8, 0x28 | 0x80, 2)
+        # convert from Gs to m / s ^ 2 and adjust for the range
+        x = int.from_bytes(x, 'big')
+        x = (x / divider) * STANDARD_GRAVITY
+        # y = (y / divider) * STANDARD_GRAVITY
+        # z = (z / divider) * STANDARD_GRAVITY
+        return x
--- p1/src/sensor/main.py
+++ p1/src/sensor/main.py
@@ -1,4 +1,8 @@
 from adafruit import Keleido
+from lis3dh import LIS3DH
 
 magic = Keleido(wifiName="EEERover", wifiPasswd="exhibition", topic="keleido/flex", BrokerIP = "192.168.0.10")
 magic.broadcastString("this is a message")
+
+Lis3dh = LIS3DH()
+print(Lis3dh.acceleration())
