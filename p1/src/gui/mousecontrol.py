e55f1ccf1fb04d3655ac67bcc4444bfb73606ea0
zl6114 <zl6114@ic.ac.uk>
2018-02-13 18:53:02 +0000

    working on mouse GUI

--- p1/src/gui/mousecontrol.py
+++ p1/src/gui/mousecontrol.py
@@ -1,5 +1,6 @@
 import pyautogui
-import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as mqtt
+import json
 #pyautogui.FAILSAFE = True
 
 #get the screen size
@@ -7,24 +8,36 @@ import paho.mqtt.client as mqtt
 #pyautogui.size()
 #2560,1440
 
+
 width, height = pyautogui.size()
 
 pyautogui.alert(text='would you like to connect to ESP8266?', title='connection', button='OK')
 
+data = mqtt.simple("keleido/acc",hostname = "192.168.0.10")
+print(str(data.payload,'utf-8'))
 # # The callback for when the client receives a CONNACK response from the server.
 # def on_connect(client, userdata, flags, rc):
 #     print("Connected with result code "+str(rc))
 #
 #     # Subscribing in on_connect() means that if we lose the connection and
 #     # reconnect then subscriptions will be renewed.
-#     client.subscribe("keleido/flex")
+#     client.subscribe("keleido/acc")
 #
 # # The callback for when a PUBLISH message is received from the server.
 # def on_message(client, userdata, msg):
-#     print(msg.topic+" "+str(msg.payload))
-#     print(msg.payload)
-#     # if int(str(msg.payload)) >= 10:
-#     #     print("click")
+#     #print(msg.topic+" "+ str(msg.payload,'utf-8'))
+#     data = json.loads(str(msg.payload,'utf-8'))
+#     # print(data['angle'])
+#     # if data['angle'] <= 60:
+#     #     pyautogui.keyDown('space')
+#     # else :
+#     #     pyautogui.keyUp('space')
+#     return msg
+#     # elif down == True:
+#     #     down = False
+#     #     pyautogui.typewrite('H')
+#     #     pyautogui.PAUSE = 1
+#
 #
 # client = mqtt.Client()
 # client.on_connect = on_connect
@@ -32,16 +45,16 @@ pyautogui.alert(text='would you like to connect to ESP8266?', title='connection'
 #
 # client.connect("192.168.0.10", 1883, 60)
 #
-# # Blocking call that processes network traffic, dispatches callbacks and
-# # handles reconnecting.
-# # Other loop*() functions are available that give a threaded interface and a
-# # manual interface.
-# client.loop_forever()
-
+# # client.loop_forever()
+#
+# run = True
+# while run:
+#     client.loop()
+#     print(client.msg())
 # pyautogui.PAUSE = 2
 #
-pyautogui.click(601,527)
-pyautogui.typewrite('Hello world!', 0.25)
+# pyautogui.click(601,527)
+# pyautogui.typewrite('Hello world!', 0.25)
 #
 # for i in range (10):
 #     pyautogui.press('space')
