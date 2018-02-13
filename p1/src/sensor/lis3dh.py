26521d47e1a6e1f6c443d3ee3f9e2f9049996af0
zl6114 <zl6114@ic.ac.uk>
2018-02-13 15:15:24 +0000

    data reading done on lis3dh

--- p1/src/sensor/lis3dh.py
+++ p1/src/sensor/lis3dh.py
@@ -10,21 +10,21 @@ import ustruct
 
 # Register value constants:
 # refrenece to
-# RANGE_16_G               = const(0b11)    # +/- 16g
-# RANGE_8_G                = const(0b10)    # +/- 8g
-# RANGE_4_G                = const(0b01)    # +/- 4g
-# RANGE_2_G                = const(0b00)    # +/- 2g (default value)
-# DATARATE_1344_HZ         = const(0b1001)  # 1.344 KHz
-# DATARATE_400_HZ          = const(0b0111)  # 400Hz
-# DATARATE_200_HZ          = const(0b0110)  # 200Hz
-# DATARATE_100_HZ          = const(0b0101)  # 100Hz
-# DATARATE_50_HZ           = const(0b0100)  # 50Hz
-# DATARATE_25_HZ           = const(0b0011)  # 25Hz
-# DATARATE_10_HZ           = const(0b0010)  # 10 Hz
-# DATARATE_1_HZ            = const(0b0001)  # 1 Hz
-# DATARATE_POWERDOWN       = const(0)
-# DATARATE_LOWPOWER_1K6HZ  = const(0b1000)
-# DATARATE_LOWPOWER_5KHZ   = const(0b1001)
+RANGE_16_G               = const(0b11)    # +/- 16g
+RANGE_8_G                = const(0b10)    # +/- 8g
+RANGE_4_G                = const(0b01)    # +/- 4g
+RANGE_2_G                = const(0b00)    # +/- 2g (default value)
+DATARATE_1344_HZ         = const(0b1001)  # 1.344 KHz
+DATARATE_400_HZ          = const(0b0111)  # 400Hz
+DATARATE_200_HZ          = const(0b0110)  # 200Hz
+DATARATE_100_HZ          = const(0b0101)  # 100Hz
+DATARATE_50_HZ           = const(0b0100)  # 50Hz
+DATARATE_25_HZ           = const(0b0011)  # 25Hz
+DATARATE_10_HZ           = const(0b0010)  # 10 Hz
+DATARATE_1_HZ            = const(0b0001)  # 1 Hz
+DATARATE_POWERDOWN       = const(0)
+DATARATE_LOWPOWER_1K6HZ  = const(0b1000)
+DATARATE_LOWPOWER_5KHZ   = const(0b1001)
 
 REG_OUTADC1_L   = const(0x08)
 REG_WHOAMI      = const(0x0F)
@@ -34,6 +34,7 @@ REG_CTRL3       = const(0x22)
 REG_CTRL4       = const(0x23)
 REG_CTRL5       = const(0x24)
 REG_OUT_X_H     = const(0x29)
+REG_OUT_X_L     = const(0x28)
 FIFO_CTRL_REG   = const(0x2E)
 INT1_CFG        = const(0x30)
 REG_INT1SRC     = const(0x31)
@@ -54,54 +55,37 @@ class LIS3DH:
         self.int2 = int2
         #set up I2C
         self.i2c_lis3dh = I2C(scl=Pin(int1), sda=Pin(int2), freq=100000)
-
+        #scan for i2cportNo
         i2cportNo = self.i2c_lis3dh.scan()
-        ADSAddr = i2cportNo[0]
-        # Set Reboot flag of Control 5 Register (0x24) high
-        self.i2c_lis3dh.writeto_mem(ADSAddr,0x24,bytearray(0x80))
-        time.sleep(0.01) # takes 5ms
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL3,bytearray(0x10))
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL5,bytearray(0x08))
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL3,bytearray(0x80))
-        # # Enable all axes, normal mode, REG_CTRL1
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,0x20,bytearray(0x77))
-        # # High res & BDU enabled. REG_CTRL4
-        # # RANGE_2_G
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,0x23,bytearray(0x88))
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL3,bytearray(0x10))
-        # # Enable ADCs.REG_TEMPCFG
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,0x1F,bytearray(0x80))
-        # # Latch interrupt for INT1
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,0x24,bytearray(0x00))
-        # # Set interrupt
-        # self.i2c_lis3dh.writeto_mem(ADSAddr,0x2E,bytearray(0x40))
-
+        AccAddr = i2cportNo[0]
+        # Enable all axes, normal mode, REG_CTRL1
+        self.i2c_lis3dh.writeto_mem(AccAddr,REG_CTRL1,bytearray([0x97]))
+        # High res & BDU enabled. REG_CTRL4
+        # RANGE_2_G
+        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL4,bytearray([0x88]))
 
     def acceleration(self):
         i2cportNo = self.i2c_lis3dh.scan()
-        ADSAddr = i2cportNo[0]
-        # Enable all axes, normal mode, REG_CTRL1
-        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL1,bytearray(0x77))
-        # High res & BDU enabled. REG_CTRL4
-        # RANGE_2_G
-        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL4,bytearray(0x88))
-        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL3,bytearray(0x10))
-        # Enable ADCs.REG_TEMPCFG
-        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_TEMPCFG,bytearray(0x80))
-        # Latch interrupt for INT1
-        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL5,bytearray(0x00))
-        # Set interrupt
-        self.i2c_lis3dh.writeto_mem(ADSAddr,FIFO_CTRL_REG,bytearray(0x40))
-        # interrupt int1 config
-        self.i2c_lis3dh.writeto_mem(ADSAddr,INT1_CFG,bytearray(0b11111111))
-        self.i2c_lis3dh.writeto_mem(ADSAddr,REG_INT1SRC,bytearray(0b01111111))
-        #self.i2c_lis3dh.writeto_mem(ADSAddr,0x2E,bytearray(0x40))
-        #The x, y, z acceleration values returned in a 3-tuple and are in m / s ^ 2.
+        AccAddr = i2cportNo[0]
         divider = 16380
-        x = self.i2c_lis3dh.readfrom_mem(ADSAddr, REG_OUT_X_H, 2)
+        x = self.i2c_lis3dh.readfrom_mem(AccAddr, REG_OUT_X_L  | 0x80, 2)
         # convert from Gs to m / s ^ 2 and adjust for the range
         x = int.from_bytes(x, 'big')
-        # x = (x / divider) * STANDARD_GRAVITY
+        x = (x / divider) * STANDARD_GRAVITY
         # y = (y / divider) * STANDARD_GRAVITY
         # z = (z / divider) * STANDARD_GRAVITY
         return x
+
+        # Enable all axes, normal mode, REG_CTRL1
+        # self.i2c_lis3dh.writeto_mem(AccAddr,REG_CTRL1,bytearray(0x97))
+        # self.i2c_lis3dh.writeto_mem(AccAddr,REG_CTRL4,bytearray(0x08))
+        # # High res & BDU enabled. REG_CTRL4
+        # # RANGE_2_G
+        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL4,bytearray(0x88))
+        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL3,bytearray(0x10))
+        # # Enable ADCs.REG_TEMPCFG
+        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_TEMPCFG,bytearray(0x80))
+        # # Latch interrupt for INT1
+        # self.i2c_lis3dh.writeto_mem(ADSAddr,REG_CTRL5,bytearray(0x00))
+        # # Set interrupt
+        # self.i2c_lis3dh.writeto_mem(ADSAddr,FIFO_CTRL_REG,bytearray(0x40))
--- p1/src/sensor/main.py
+++ p1/src/sensor/main.py
@@ -1,5 +1,6 @@
 from adafruit import Keleido
 from lis3dh import LIS3DH
+import time
 
 # magic = Keleido(wifiName="EEERover", wifiPasswd="exhibition", topic="keleido/flex", BrokerIP = "192.168.0.10")
 scl = 0
@@ -8,3 +9,4 @@ Lis3dh = LIS3DH(scl,sda)
 
 while 1:
     print(Lis3dh.acceleration())
+    time.sleep(0.3)
