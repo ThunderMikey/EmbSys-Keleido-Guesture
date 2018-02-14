84c24b4a86a33a7ce3b104ccdda69e49ff9bef8e
zl6114 <zl6114@ic.ac.uk>
2018-02-14 21:21:30 +0000

    upload demo files

--- p1/src/gui/PC_Moniting_Status.py
+++ p1/src/gui/PC_Moniting_Status.py
@@ -1,6 +1,6 @@
 import pyautogui
 import paho.mqtt.client as mqtt
-#pyautogui.FAILSAFE = True
+import json
 
 width, height = pyautogui.size()
 
@@ -10,33 +10,20 @@ pyautogui.alert(text='would you like to connect to ESP8266?', title='connection'
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("keleido/flex")
+    client.subscribe("keleido/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    #bytes.decode
-    #json,load
-    #str to int
-    #do somthing
-    print(msg.topic+" "+str(msg.payload))
-    pyautogui.hotkey('altleft','tab')
-    if int(msg.payload) >= 10:
-        pyautogui.hotkey('altleft','tab')
+    Data = json.loads(str(msg.payload,'utf-8'))
+    # print(flexJson)
+    print(Data)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# if int(str(client.msg.payload)) >= 10:
-#     print("click")
-
 client.connect("192.168.0.10", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
